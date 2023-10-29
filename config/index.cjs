c29e04d58ce0fb24f4fad31e548f87bd2e210507
lulululululu55555 <37288757+lulululululu55555@users.noreply.github.com>
2023-10-29 18:27:00 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,8 +13,8 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: 'd459076ae55a989357c5974dbb4b03f3',
 
-  PROVINCE: '浙江',
-  CITY: '杭州',
+  PROVINCE: '广东',
+  CITY: '惠州',
 
   USERS: [
     {
